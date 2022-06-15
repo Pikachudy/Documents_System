9b12cc19b5263741418f3d1c78b064576e350563
JiaoJiao <923252409@qq.com>
2022-06-16 01:16:43 +0800

    还剩下文件删除、路径跳转、属性

--- src/main.js
+++ src/main.js
@@ -4,11 +4,13 @@ import {
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+import Menus from 'vue3-menus';
 import App from './App.vue'
 
 const app = createApp(App)
 
 app.use(ElementPlus)
+app.use(Menus)
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
     app.component(key, component)
 }
